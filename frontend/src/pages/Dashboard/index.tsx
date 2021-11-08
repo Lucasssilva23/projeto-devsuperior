e6bd8997663cc4ccdd146bb75c5dfc3b2b1f5550
Lucas Silva <Lucasssilva2912@gmail.com>
2021-11-07 22:35:06 -0300

    Home, navigation

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -6,35 +6,35 @@ import NavBar from "components/NavBar";
 
 const Dashboard = () => {
     return (
-       <>
-      <NavBar />
-      <div className='container'>
+        <>
+            <NavBar />
+            <div className='container'>
 
-        <h1 className="text-primary py-3" >Dashboard de Vendas</h1>
+                <h1 className="text-primary py-3" >Dashboard de Vendas</h1>
 
-        <div className="row px-3">
+                <div className="row px-3">
 
-          <div className="col-sm-6">
-            <h5 className="text-center text-secondary" >Taxa de sucesso (%)</h5>
-            <BarChart/>
-          </div>
+                    <div className="col-sm-6">
+                        <h5 className="text-center text-secondary" >Taxa de sucesso (%)</h5>
+                        <BarChart />
+                    </div>
 
-          <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Todas as vendas</h5>
-            <DonutChart />
-          </div>
+                    <div className="col-sm-6">
+                        <h5 className="text-center text-secondary">Todas as vendas</h5>
+                        <DonutChart />
+                    </div>
 
-          <div className="py-3">
-            <h2 className="text-primary">Todas as vendas</h2>
-          </div>
+                    <div className="py-3">
+                        <h2 className="text-primary">Todas as vendas</h2>
+                    </div>
 
-        </div>
+                </div>
 
-        <DataTable/>
-      </div>
+                <DataTable />
+            </div>
 
-      <Footer />
-    </>
+            <Footer />
+        </>
     );
 }
 
